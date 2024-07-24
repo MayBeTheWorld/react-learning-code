02a55a3e98dd174c970623ee04d788b3387df8b1
MayBeTheWorld <13265252344@163.com>
2024-07-24 16:48:02 +0800

    the usage of hook useState

--- hook-test/src/App.tsx
+++ hook-test/src/App.tsx
@@ -1,10 +1,17 @@
+import { resolve } from "path";
 import { useState } from "react";
 
 function App() {
-  const [num, setNum] = useState(1);
+  // useState 返回一个数组，包含 state 和 setXxx 的 api，一般我们都是用解构语法取
+  const [num, setNum] = useState(() => {
+    const num1 = 1 + 2
+    const num2 = 2 + 3
+    return num1 + num2
+  });
 
   return (
-    <div onClick={() => setNum(num + 1)}>{num}</div>
+    // setXxx 的 api, 可以直接传新的值，或者传一个函数，返回新的值(这个函数的参数是上一次的 state)
+    <div onClick={() => setNum((prevNum) => prevNum + 1)}>{num}</div>
   );
 }
 
